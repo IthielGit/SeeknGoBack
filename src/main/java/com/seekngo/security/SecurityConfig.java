58801f022edf7c7f8f69cc436b5da5e9f964225e
Ithiel <harinosy.tradeco@gmail.com>
2022-08-08 22:14:08 +0300

    login : customAuthProvider

--- src/main/java/com/seekngo/security/SecurityConfig.java
+++ src/main/java/com/seekngo/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -16,6 +17,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private CustomUserDetailsService userDetailsService;
+	
+	@Autowired
+	private CustomAuthenticationProvider authenticationProvider;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -34,19 +38,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //		auth.inMemoryAuthentication()
 //		.withUser("Admin").password("1234").roles("ADMIN","USER");
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+
+		/*USING USERDETAILS SERVICE
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());*/
+		
+		auth.authenticationProvider(authenticationProvider);
+		
 	}
 	
+	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+//		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
+
 	}
 	
+	/* if using default Userdetails => authenticationManager tsy misy bean
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}*/
 	
 	@Bean
 	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		// TODO Auto-generated method stub
-		return super.authenticationManager();
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
